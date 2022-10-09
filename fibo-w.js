6fa3dad583575d6220458c3db30fbc47e69826c8
Joel Dietz <jdietz@gmail.com>
2022-10-09 19:50:36 +0400

    more attempts at fib text writer

---
+++ fibo-w.js
@@ -0,0 +1,56 @@
+// source: https://github.com/dherault/fibonacci_spiral
+//
+var texts = ["","","","","","","i","i am","i breathe", "i am joel", "i knew my own name", "I wrote a book about aliens", "I learned to code in Pascal", "I designed games", "I wrote poems" ]
+const fcanvas = document.getElementById('layer1')
+
+var coords = [];
+
+fcanvas.width = 800;
+fcanvas.height = 600;
+
+const { width, height } = fcanvas
+
+const _ = fcanvas.getContext('2d')
+
+function drawSquareAndQuarterCircle(size) {
+  _.beginPath()
+  _.rect(0, 0, size, size)
+  // _.arc(0, 0, size, 0, Math.PI / 2)
+  // _.write("hello")
+  _.stroke()
+}
+
+let size = 1
+let previousSize = size
+
+_.lineWidth = 1
+_.strokeStyle = 'black'
+
+_.translate(width / 2, height / 2)
+// _.rotate(-Math.PI / 2)
+
+for (let i = 0; i < 15; i++) {
+  drawSquareAndQuarterCircle(size)
+  coords.push(size);
+  console.log(coords);
+  try {
+  _.fillText(texts[i], 10, 10);
+} catch {
+
+}
+  const tempSize = previousSize
+  previousSize = size
+  size += tempSize
+
+  // if(i==4)
+  //   _.fillText("middle way", _.width,_.height);
+  //
+  //
+  // if(i==10) {
+  //   console.log(size);
+  //   _.fillText("middle way", _.width,_.height);
+  //   }
+
+  _.translate(-tempSize, 0)
+  _.rotate(-Math.PI / 2)
+}
