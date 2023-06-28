847adccd3b25801ad148c8959dddf3eab3f763c0
Joel Dietz <jdietz+github@gmail.com>
2023-06-28 03:39:34 +0200

    Now restarts if goes stable

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -11,11 +11,14 @@ for (let i = 0; i < gsize; i++) {
     }
 }
 
+let stableCount = 0;
+let previousStates = Array(10).fill().map(() => JSON.stringify(grid));
+
 function gameOfLife() {
     let nextGrid = JSON.parse(JSON.stringify(grid));  // Copy the current grid
 
     for (let i = 0; i < gsize; i++) {
-        for (let j = 0; j < gsize; j++) {
+        for (let j = 0; j <gsize; j++) {
             let neighbors = getNeighborCount(i, j);
 
             if (grid[i][j] === 1 && (neighbors < 2 || neighbors > 3)) {
@@ -26,10 +29,41 @@ function gameOfLife() {
         }
     }
 
+    let nextGridString = JSON.stringify(nextGrid);
+
+    previousStates.shift();  // Remove the oldest state
+    previousStates.push(nextGridString);  // Add the new state
+
+    // Check if the new state has appeared before in the last 10 turns
+    if (previousStates.slice(0, -1).includes(nextGridString)) {
+        stableCount++;
+    } else {
+        stableCount = 0;
+    }
+
+    // If the grid has been stable for 10 turns, introduce variation
+    if (stableCount >= 10) {
+        introduceVariation(nextGrid);
+        stableCount = 0;
+        previousStates = Array(10).fill().map(() => JSON.stringify(nextGrid));  // Reset the stored states
+    }
+
     grid = nextGrid;  // Update the grid with the next generation
 
     drawGrid();
     requestAnimationFrame(gameOfLife);
+}
+
+// The introduceVariation function remains the same as before
+
+function introduceVariation(grid) {
+    console.log("flipping");
+    for (let i = 0; i < 30; i++) {  // Randomly flip 10 cells
+        let x = Math.floor(Math.random() * gsize);
+        let y = Math.floor(Math.random() * gsize);
+
+        grid[x][y] = grid[x][y] === 0 ? 1 : 0;
+    }
 }
 
 function getNeighborCount(x, y) {
