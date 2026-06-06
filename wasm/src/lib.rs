6b562fdaaf9409be91aa7ae2c4e8cfabc46ee347
Joel Dietz <joel@swarm.com>
2026-06-05 23:56:57 -0700

    refactor: improve wasm/src/lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,9 @@
 use wasm_bindgen::prelude::*;
 use serde::{Serialize, Deserialize};
 
+const FREQ_MIN: f32 = -1000.0;
+const FREQ_MAX: f32 = 1000.0;
+
 #[wasm_bindgen]
 #[derive(Serialize, Deserialize)]
 pub struct LifeForm {
@@ -8,6 +11,14 @@ pub struct LifeForm {
     pub y_freq: f32,
     pub z_freq: f32,
     pub mutation: f32,
+}
+
+fn sanitize_freq(value: f32) -> f32 {
+    if value.is_nan() {
+        0.0
+    } else {
+        value.clamp(FREQ_MIN, FREQ_MAX)
+    }
 }
 
 #[wasm_bindgen]
@@ -19,17 +30,25 @@ impl LifeForm {
 
     #[wasm_bindgen]
     pub fn mutate(&mut self) {
-        self.x_freq += (js_sys::Math::random() as f32 - 0.5) * self.mutation;
-        self.y_freq += (js_sys::Math::random() as f32 - 0.5) * self.mutation;
-        self.z_freq += (js_sys::Math::random() as f32 - 0.5) * self.mutation;
+        let dx = (js_sys::Math::random() as f32 - 0.5) * self.mutation;
+        let dy = (js_sys::Math::random() as f32 - 0.5) * self.mutation;
+        let dz = (js_sys::Math::random() as f32 - 0.5) * self.mutation;
+
+        self.x_freq = sanitize_freq(self.x_freq + dx);
+        self.y_freq = sanitize_freq(self.y_freq + dy);
+        self.z_freq = sanitize_freq(self.z_freq + dz);
     }
 }
 
 #[wasm_bindgen]
-pub fn step_simulation(json_state: &str) -> String {
-    let mut life_forms: Vec<LifeForm> = serde_json::from_str(json_state).unwrap_or_default();
+pub fn step_simulation(json_state: &str) -> Result<String, JsValue> {
+    let mut life_forms: Vec<LifeForm> = serde_json::from_str(json_state)
+        .map_err(|e| JsValue::from_str(&format!("Deserialization error: {}", e)))?;
+
     for lf in &mut life_forms {
         lf.mutate();
     }
-    serde_json::to_string(&life_forms).unwrap()
+
+    serde_json::to_string(&life_forms)
+        .map_err(|e| JsValue::from_str(&format!("Serialization error: {}", e)))
 }
